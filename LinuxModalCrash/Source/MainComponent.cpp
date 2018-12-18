0d55db2679d86d706dae25bb9086f47e826f3ca8
Roland Rabien <roland@rabien.com>
2018-12-18 08:41:28 -0800

    Trying to reproduce linux crash

---
+++ LinuxModalCrash/Source/MainComponent.cpp
@@ -0,0 +1,61 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+  ==============================================================================
+*/
+
+#include "MainComponent.h"
+
+class AnotherWindow : public TopLevelWindow
+{
+public:
+    AnotherWindow() : TopLevelWindow ("Another Window", true)
+    {
+        setVisible (true);
+        setAlwaysOnTop (true);
+        toFront (true);
+        centreWithSize (500, 400);
+    }
+    
+    void paint (Graphics& g) override
+    {
+        g.fillAll (Colours::pink);
+    }
+};
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    setSize (600, 400);
+    
+    Timer::callAfterDelay (100, [&]
+                           {
+                               AlertWindow::showOkCancelBox (AlertWindow::NoIcon, "Please Wait", "Please Wait");
+                           });
+    
+    Timer::callAfterDelay (500, [&]
+                           {
+                               aw = new AnotherWindow();
+                           });
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
+
+    g.setFont (Font (16.0f));
+    g.setColour (Colours::white);
+    g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+}
+
+void MainComponent::resized()
+{
+}
