f3265b9a80b8c2b318babc41d10fa358c5312582
Roland Rabien <roland@rabien.com>
2019-01-03 20:35:42 -0800

    Repro for ScopedPointer deleting twice

---
+++ OSP_Crash/Source/Main.cpp
@@ -0,0 +1,39 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic startup code for a JUCE application.
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+
+
+struct Thingy
+{
+    ~Thingy();
+    int cnt = 0;
+};
+
+ScopedPointer<Thingy> thingy;
+
+Thingy::~Thingy()
+{
+    cnt++;
+    if (cnt == 2)
+        printf ("Deconstructors should only be called once!\n");
+    
+    thingy.reset();
+}
+
+int main (int argc, char* argv[])
+{
+    thingy.reset (new Thingy());
+    thingy.reset();
+    
+    return 0;
+}
