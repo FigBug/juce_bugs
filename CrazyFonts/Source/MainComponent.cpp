91e37dc9d402e726c917d556df3acc8a96b1db9c
Roland Rabien <roland@rabien.com>
2019-04-09 09:23:01 -0700

    Use even more fonts

--- CrazyFonts/Source/MainComponent.cpp
+++ CrazyFonts/Source/MainComponent.cpp
@@ -14,11 +14,29 @@ Colour goldenRatioColor (int idx)
     return Colour (float (h), 0.8f, 0.95f, 1.0f);
 }
 
+Typeface::Ptr getTypeface()
+{
+    static int idx = 0;
+    idx++;
+    
+    switch (idx % 3)
+    {
+        case 0:
+            return Typeface::createSystemTypefaceFor (BinaryData::OpenSansRegular_ttf, BinaryData::OpenSansRegular_ttfSize);
+        case 1:
+            return Typeface::createSystemTypefaceFor (BinaryData::RobotoMonoRegular_ttf, BinaryData::RobotoMonoRegular_ttfSize);
+        case 2:
+            return Typeface::createSystemTypefaceFor (BinaryData::TitilliumWebRegular_ttf, BinaryData::TitilliumWebRegular_ttfSize);
+        default:
+            return Typeface::createSystemTypefaceFor (BinaryData::TitilliumWebRegular_ttf, BinaryData::TitilliumWebRegular_ttfSize);
+    }
+}
+
 //==============================================================================
 MainComponent::MainComponent()
 {
     setSize (600, 400);
-    startTimerHz (30);
+    startTimerHz (200);
 }
 
 MainComponent::~MainComponent()
@@ -29,18 +47,28 @@ MainComponent::~MainComponent()
 void MainComponent::paint (Graphics& g)
 {
     g.fillAll (getLookAndFeel().findColour (ResizableWindow::backgroundColourId));
-
-    static int idx = 0;
-    idx++;
-
-    for (int i = 0; i < 10; i++)
+    
+    int h = getHeight() / 20;
+    int w = getWidth()  / 10;
+    
+    auto area = getLocalBounds();
+    
+    for (int j = 0; j < 10; j++)
     {
-        auto typeface = Typeface::createSystemTypefaceFor (BinaryData::OpenSansRegular_ttf,  BinaryData::OpenSansRegular_ttfSize);
-        Font f (typeface);
+        auto rc = area.removeFromLeft (w);
 
-        g.setFont (f);
-        g.setColour (goldenRatioColor (idx));
-        g.drawText ("Hello World!", getLocalBounds(), Justification::centred, true);
+        for (int i = 0; i < 20; i++)
+        {
+            auto typeface = getTypeface();
+            Font f (typeface);
+            
+            static int idx = 0;
+            idx++;
+
+            g.setFont (f);
+            g.setColour (goldenRatioColor (idx));
+            g.drawText ("Hello World!", rc.removeFromTop (h), Justification::centred, true);
+        }
     }
 }
 
