f617a543dd31800f99642a4e91d530eee6fb825d
Roland Rabien <roland@rabien.com>
2019-09-12 10:15:16 -0700

    Added OptionalScopedPointer

---
+++ OptionalScopedPointer/Source/Main.cpp
@@ -0,0 +1,34 @@
+/*
+  ==============================================================================
+
+    This file was auto-generated!
+
+    It contains the basic startup code for a JUCE application.
+
+  ==============================================================================
+*/
+
+#include "../JuceLibraryCode/JuceHeader.h"
+
+//==============================================================================
+class Foo
+{
+public:
+	~Foo()
+	{
+		DBG("I'm deleted!");
+	}	
+
+private:
+	JUCE_LEAK_DETECTOR(Foo);
+};
+
+int main (int argc, char* argv[])
+{
+	OptionalScopedPointer<Foo> foo;
+	foo.set (new Foo(), true);
+	foo.reset();
+	foo.release();
+
+    return 0;
+}
