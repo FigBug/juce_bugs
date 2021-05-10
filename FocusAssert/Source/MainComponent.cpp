ec4c3a418766ae2e27d15a5adce63e326032d720
Roland Rabien <figbug@gmail.com>
2021-05-10 14:44:17 -0700

    FocusAssert

---
+++ FocusAssert/Source/MainComponent.cpp
@@ -0,0 +1,24 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+	addAndMakeVisible (text);
+	setFocusContainer(true);
+	setSize (600, 400);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+}
+
+void MainComponent::resized()
+{
+	text.setBounds (5, 5, 200, 30);
+}
