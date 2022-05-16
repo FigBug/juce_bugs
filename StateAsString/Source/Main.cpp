b20442a1a1c905cff93edfd749cef9f40c9cb18f
Roland Rabien <figbug@gmail.com>
2022-05-16 16:11:59 -0700

    Add StateAsString

---
+++ StateAsString/Source/Main.cpp
@@ -0,0 +1,88 @@
+/*
+  ==============================================================================
+
+    This file contains the basic startup code for a JUCE application.
+
+  ==============================================================================
+*/
+
+#include <JuceHeader.h>
+#include "MainComponent.h"
+
+//==============================================================================
+class StateAsStringApplication  : public juce::JUCEApplication
+{
+public:
+    //==============================================================================
+    StateAsStringApplication() {}
+
+    const juce::String getApplicationName() override       { return ProjectInfo::projectName; }
+    const juce::String getApplicationVersion() override    { return ProjectInfo::versionString; }
+    bool moreThanOneInstanceAllowed() override             { return true; }
+
+    //==============================================================================
+    void initialise (const juce::String& commandLine) override
+    {
+        mainWindow.reset (new MainWindow (getApplicationName()));
+    }
+
+    void shutdown() override
+    {
+        mainWindow = nullptr; // (deletes our window)
+    }
+
+    //==============================================================================
+    void systemRequestedQuit() override
+    {
+        quit();
+    }
+
+    void anotherInstanceStarted (const juce::String& commandLine) override
+    {
+    }
+
+    //==============================================================================
+    class MainWindow    : public juce::DocumentWindow
+    {
+    public:
+        MainWindow (juce::String name)
+            : DocumentWindow (name,
+                              juce::Desktop::getInstance().getDefaultLookAndFeel()
+                                                          .findColour (juce::ResizableWindow::backgroundColourId),
+                              DocumentWindow::allButtons)
+        {
+            setUsingNativeTitleBar (true);
+            setContentOwned (new MainComponent(), true);
+
+            setResizable (true, true);
+			setBounds (200, 200, 1000, 1000);
+
+			juce::Timer::callAfterDelay (1000, []
+			{
+				JUCEApplication::getInstance()->systemRequestedQuit();
+			});
+        }
+
+		~MainWindow() override
+		{
+			auto str = getWindowStateAsString();
+			printf("%s\n", str.toRawUTF8());
+			jassert (juce::StringArray::fromTokens (str, " ", "").strings.getLast().getIntValue() == getHeight());
+		}
+
+        void closeButtonPressed() override
+        {
+            JUCEApplication::getInstance()->systemRequestedQuit();
+        }
+
+    private:
+        JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainWindow)
+    };
+
+private:
+    std::unique_ptr<MainWindow> mainWindow;
+};
+
+//==============================================================================
+// This macro generates the main() routine that launches the app.
+START_JUCE_APPLICATION (StateAsStringApplication)
---
+++ StateAsString/Source/MainComponent.cpp
@@ -0,0 +1,21 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    setSize (600, 400);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+}
+
+void MainComponent::resized()
+{
+}
