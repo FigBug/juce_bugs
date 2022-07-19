41c193431c10cd2a34cb3e4559ba28c6baf03702
Roland Rabien <figbug@gmail.com>
2022-07-19 08:35:24 -0700

    Stuff

---
+++ Resize Glitch/Source/MainComponent.cpp
@@ -0,0 +1,27 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    setSize (600, 400);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    g.setFont (juce::Font (16.0f));
+    g.setColour (juce::Colours::white);
+    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+}
+
+void MainComponent::resized()
+{
+	printf ( "resized\n" );
+}
