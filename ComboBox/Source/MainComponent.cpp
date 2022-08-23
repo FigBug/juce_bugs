03b43f2d796d4188473d18cf95b166a56c3b39c3
Roland Rabien <figbug@gmail.com>
2022-08-23 10:52:28 -0700

    ComboBox

---
+++ ComboBox/Source/MainComponent.cpp
@@ -0,0 +1,43 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+	startTimer (1000);
+    setSize (600, 400);
+
+	addAndMakeVisible(box);
+	auto& m = *box.getRootMenu();
+	m.addItem (1, "a");
+	m.addItem ("b", [] {});
+	m.addItem (2, "c");
+	m.addItem ("d", [] {});
+	m.addItem (3, "e");
+	m.addItem ("f", [] {});
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+void MainComponent::timerCallback()
+{
+	static int count = 0;
+
+	juce::StringArray str = { "a", "b", "c", "d", "e", "f" };
+
+	box.setText (str[count % str.size()]);
+
+	count++;
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+}
+
+void MainComponent::resized()
+{
+	box.setBounds (10, 10, 200, 24);
+}
