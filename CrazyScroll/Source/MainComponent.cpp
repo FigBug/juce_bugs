3553b4ae98a7778d373a0e00f262e7aaa55a7ba5
Roland Rabien <figbug@gmail.com>
2022-10-31 12:54:08 -0700

    Crazy scroll

---
+++ CrazyScroll/Source/MainComponent.cpp
@@ -0,0 +1,50 @@
+#include "MainComponent.h"
+
+class Frame : public juce::Component
+{
+public:
+	Frame()
+	{
+		setSize(2000, 100);
+
+		for (int i = 0; i < 20; i++)
+		{
+			auto b = new juce::TextButton ("hi");
+			b->setBounds(i * 100, 0, 100, 24);
+			b->onClick = [b]
+			{
+				delete b;
+			};
+			addAndMakeVisible (b);
+		}
+	}
+
+	~Frame() override
+	{
+		deleteAllChildren();
+	}
+};
+
+//==============================================================================
+MainComponent::MainComponent()
+{
+    setSize (600, 400);
+	addAndMakeVisible (viewport);
+	viewport.setViewedComponent (new Frame());
+	viewport.setScrollOnDragMode (juce::Viewport::ScrollOnDragMode::all);
+}
+
+MainComponent::~MainComponent()
+{
+}
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+}
+
+void MainComponent::resized()
+{
+	viewport.setBounds (getLocalBounds());
+}
