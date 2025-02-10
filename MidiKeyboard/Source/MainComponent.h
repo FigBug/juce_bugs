4595928049730fe947746ba7e55f33a4b6b3ddf7
Roland Rabien <roland@rabien.com>
2025-02-09 18:14:56 -0800

    simplify

--- MidiKeyboard/Source/MainComponent.h
+++ MidiKeyboard/Source/MainComponent.h
@@ -19,13 +19,11 @@ public:
         return 10;
     }
 
-    juce::Array<int> highlightedNotes, pressedNotes;
-
     void paint (juce::Graphics& g) override
     {
-        g.fillAll (juce::Colours::white);
+        bool rectlist = true;
 
-        juce::Array<int> selectedNotes;
+        g.fillAll (juce::Colours::white);
 
         const int noteH = juce::jmax (2, getNoteHeight());
 
@@ -61,10 +59,20 @@ public:
             }
 
             g.setColour (juce::Colours::yellow.withAlpha (1.0f));
-            g.fillRectList (highlights);
+            
+            if (rectlist)
+                g.fillRectList (highlights);
+            else
+                for (auto r : highlights)
+                    g.fillRect(r);
 
             g.setColour (juce::Colours::black.withAlpha (0.8f));
-            g.fillRectList (lines);
+
+            if (rectlist)
+                g.fillRectList (lines);
+            else
+                for (auto r : lines)
+                    g.fillRect(r);
         }
 
         y = topY;
@@ -72,35 +80,7 @@ public:
         for (int octave = 0; octave < 128; octave += 12)
         {
             const float nextOctaveY = (float) noteToY (octave + 12);
-            const float dwy = (nextOctaveY - y) / 7.0f;
-            float wy = y;
-            int n = 0;
-
-            for (int white = 0; white < 7; ++white)
-            {
-                const int note = octave + reorderedNotes[n++];
-                if (selectedNotes.contains (note))
-                {
-                    g.setColour (juce::Colours::pink.withAlpha (highlightedNotes.contains (note) ? 0.6f : 0.3f));
-                    g.fillRect (1.0f, wy - std::abs (dwy) + 0.5f, getWidth() - 2.0f, std::abs (dwy) - 0.5f);
-                }
-                else if (highlightedNotes.contains (note))
-                {
-                    g.setColour (juce::Colours::orange);
-                    g.fillRect (1.0f, wy - std::abs (dwy) + 0.5f, getWidth() - 2.0f, std::abs (dwy) - 0.5f);
-                }
-
-                if (pressedNotes.contains (note))
-                {
-                    auto rc = juce::Rectangle<float> (1.0f, wy - std::abs (dwy) + 0.5f, getWidth() - 2.0f, std::abs (dwy) - 0.5f);
-                    rc = rc.removeFromRight (rc.getHeight()).reduced (1.5f);
-
-                    g.setColour (juce::Colours::black.withAlpha (0.8f));
-                    g.fillEllipse (rc);
-                }
-
-                wy += dwy;
-            }
+            int n = 7;
 
             const int bw = proportionOfWidth (0.6f);
 
@@ -113,46 +93,15 @@ public:
                 if (! getLocalBounds().expanded (noteH).getVerticalRange().contains (ypos))
                     continue;
 
-                if (selectedNotes.contains (note))
-                {
-                    g.setColour (juce::Colours::pink.withAlpha (highlightedNotes.contains (note) ? 0.6f : 0.3f));
-                    g.fillRect (r);
-                }
-                else if (highlightedNotes.contains (note))
-                {
-                    g.setColour (juce::Colours::orange);
-                    g.fillRect (r);
-                }
-                else
-                {
-                    g.setColour (juce::Colours::black);
-                    g.fillRect (r);
+                g.setColour (juce::Colours::black);
+                g.fillRect (r);
 
-                    g.setColour (juce::Colours::yellow.withAlpha (0.16f));
-                    g.drawRect (r.reduced (1).withTrimmedRight (-2).translated (-2, 0));
-                }
+                g.setColour (juce::Colours::yellow.withAlpha (0.16f));
+                g.drawRect (r.reduced (1).withTrimmedRight (-2).translated (-2, 0));
 
                 g.setColour (juce::Colours::black);
                 g.drawRect (r.withTrimmedRight (-1).translated (-1, 0));
-
-                if (pressedNotes.contains (note))
-                {
-                    auto rc = r.toFloat();
-                    rc = rc.removeFromRight (rc.getHeight()).reduced (1.5f);
-
-                    g.setColour (selectedNotes.contains (note) ? juce::Colours::black.withAlpha (0.8f) : juce::Colours::yellow.withAlpha (1.0f));
-                    g.fillEllipse (rc);
-                }
             }
-
-            auto noteName = juce::MidiMessage::getMidiNoteName (octave, true, true, 4);
-            g.setColour (juce::Colours::black);
-
-            auto tx = proportionOfWidth (0.7f);
-            auto ty = (int) (y - noteH / 3);
-
-            if (noteH > 5)
-                g.drawSingleLineText (noteName, tx, ty);
 
             y = nextOctaveY;
         }
