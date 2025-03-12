01b96f795d538db54d70445ba27a5f306c34d812
Roland Rabien <figbug@gmail.com>
2025-03-12 10:04:43 -0700

    Try #2

--- TextGrad/Source/MainComponent.cpp
+++ TextGrad/Source/MainComponent.cpp
@@ -15,17 +15,19 @@ void MainComponent::paint (juce::Graphics& g)
 {
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
+    auto bounds = getLocalBounds().withSizeKeepingCentre (300, 200);
+
 	auto c1 = juce::Colours::blue;
 	auto c2 = juce::Colours::green;
 
-	auto gradRect = getLocalBounds().withTrimmedLeft (getWidth() / 2);
+	auto gradRect = bounds.withTrimmedLeft (bounds.getWidth() / 2);
 	auto grad = juce::ColourGradient::horizontal (c1, c2, gradRect);
 
 	g.setGradientFill (grad);
 	g.setFont (juce::Font (juce::FontOptions(40)));
-    g.drawText ("Hello World! Hello World! Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
 
-	g.fillRect (0, getHeight() / 2 + 30, getWidth(), 30);
+	g.fillRect (bounds.getX(), bounds.getCentreY() + 30, bounds.getWidth(), 30);
 }
 
 void MainComponent::resized()
