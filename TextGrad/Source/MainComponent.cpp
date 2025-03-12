98e139e9b7f7723e445566ae813cb34ee02d1291
Roland Rabien <figbug@gmail.com>
2025-03-12 14:30:09 -0700

    Try #3

--- TextGrad/Source/MainComponent.cpp
+++ TextGrad/Source/MainComponent.cpp
@@ -3,6 +3,7 @@
 //==============================================================================
 MainComponent::MainComponent()
 {
+    addAndMakeVisible(test);
     setSize (650, 400);
 }
 
@@ -14,23 +15,9 @@ MainComponent::~MainComponent()
 void MainComponent::paint (juce::Graphics& g)
 {
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
-
-    auto bounds = getLocalBounds().withSizeKeepingCentre (300, 200);
-
-	auto c1 = juce::Colours::blue;
-	auto c2 = juce::Colours::green;
-
-	auto gradRect = bounds.withTrimmedLeft (bounds.getWidth() / 2);
-	auto grad = juce::ColourGradient::horizontal (c1, c2, gradRect);
-
-	g.setGradientFill (grad);
-	g.setFont (juce::Font (juce::FontOptions(40)));
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
-
-	g.fillRect (bounds.getX(), bounds.getCentreY() + 30, bounds.getWidth(), 30);
 }
 
 void MainComponent::resized()
 {
-
+    test.setBounds (getLocalBounds().withSizeKeepingCentre (getWidth()/2, getHeight()/2));
 }
--- TextGrad/Source/MainComponent.h
+++ TextGrad/Source/MainComponent.h
@@ -2,6 +2,46 @@
 
 #include <JuceHeader.h>
 
+class Test : public juce::Component
+{
+public:
+    Test ()
+    {
+    }
+
+    void paint (juce::Graphics& g)
+    {
+        auto bounds = getLocalBounds();
+
+        g.setColour (juce::Colours::white);
+        g.drawRect (getLocalBounds(), 2);
+
+        g.reduceClipRegion (bounds);
+
+	    auto c1 = juce::Colours::red;
+	    auto c2 = juce::Colours::green;
+
+	    auto gradRect = bounds.withSizeKeepingCentre (bounds.getWidth() / 2, bounds.getHeight());
+        g.setColour (juce::Colours::white.withAlpha (0.2f));
+        g.drawRect (gradRect, 2);
+
+	    auto grad1 = juce::ColourGradient::horizontal (c1, c2, gradRect);
+        auto grad2 = juce::ColourGradient::horizontal (c1, c2, getLocalBounds()); 
+
+        grad2.addColour (gradRect.getX() / float (bounds.getWidth()), c1);
+        grad2.addColour (gradRect.getRight() / float (bounds.getWidth()), c2);
+
+	    g.setFont (juce::Font (juce::FontOptions(40)));
+
+        auto rc = getLocalBounds();
+
+        g.setGradientFill (grad1);
+        g.drawText ("Hello World! Hello World!", rc.removeFromTop (rc.getHeight() / 2), juce::Justification::centred, false);
+        g.setGradientFill (grad2);
+        g.drawText ("Hello World! Hello World!", rc, juce::Justification::centred, false);
+    }
+};
+
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
@@ -19,8 +59,7 @@ public:
     void resized() override;
 
 private:
-    //==============================================================================
-    // Your private member variables go here...
+    Test test;
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
